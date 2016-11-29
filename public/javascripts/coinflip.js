daca87990ce8964dae7dca552b7e0cf44e8e7519
Mason Prey <masonprey7@gmail.com>
2016-11-28 22:23:47 -0600

    dropdown history menus for coinflip page

---
+++ public/javascripts/coinflip.js
@@ -0,0 +1,19 @@
+$(function() {
+
+  $('.header').on('click', '#cf-dropdown-trigger', function(event) {
+    var header = $(this).parent().parent();
+    $(this).hide();
+    if ($(this).hasClass('opened')) {
+      $(this).next().show();
+    } else {
+      $(this).prev().show();
+    }
+    header.addClass('border-bottom');
+    $(this).parent().parent().parent().find('.table-wrapper').slideToggle(function() {
+      if (!($(this).is(':visible'))) {
+        header.removeClass('border-bottom');
+      }
+    });
+  });
+
+});
