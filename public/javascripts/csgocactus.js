2de75f6c4459f7b31f9e545b9f2dadb39067c6e4
Mason Prey <masonprey7@gmail.com>
2016-12-05 00:42:59 -0600

    front-end reload button for balance, add toastr.js, start back-end credit balancing

--- public/javascripts/csgocactus.js
+++ public/javascripts/csgocactus.js
@@ -1,15 +1,21 @@
 $(function() {
 
-  var socket_outgoing = {
+  $balance_reload = $('#balance-reload');
+  $balance_label = $('#balance-label');
 
+  var socket_outgoing = {
+    REQUEST_UPDATE_BALANCE: 'REQUEST_UPDATE_BALANCE'
   };
 
   var socket_incoming = {
     ALERT: 'ALERT',
-    FORCE_RELOAD: 'FORCE_RELOAD'
+    FORCE_RELOAD: 'FORCE_RELOAD',
+    UPDATE_BLANE: 'UPDATE_BALANCE'
   };
 
   var socket = io.connect();
+
+  toastr.options.closeButton = true;
 
   socket.on(socket_incoming.ALERT, function(data) {
     swal(data.header, data.message, data.type);
@@ -18,6 +24,18 @@ $(function() {
   socket.on(socket_incoming.FORCE_RELOAD, function() {
     location.reload();
   });
+
+  $balance_reload.on('click', function() {
+    $balance_reload.toggleClass('animate');
+  });
+
+  function countUpBalance(newBalance) {
+    var existingBalance = 0;
+    if ($.isNumeric($balance_label.text().replace(/,/g, ''))) {
+      existingBalance = Number($balance_label.text().replace(/,/g, ''));
+    }
+    new CountUp('balance-label', existingBalance, newBalance, 2, 2.5).start();
+  }
 
   window.socket = socket;
 
